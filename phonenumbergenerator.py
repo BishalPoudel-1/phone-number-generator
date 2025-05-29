fd36659b7dfc68b48fb112ffda42abf9482f824a
BishalPoudel-1 <bishalpoudel884@gmail.com>
2025-05-29 22:17:44 +0545

    code change

--- phonenumbergenerator.py
+++ phonenumbergenerator.py
@@ -1,8 +1,10 @@
-print("**********************************")
+print("*************************************")
 print('\tPhone Number Generator')
-print("**********************************\n")
+print("*************************************\n")
 min = int(input("Enter the minimum Digits: "))
 max = int(input("Enter the maximum Digits: "))
 firstdigit = int(input('Enter the First Digit of Phone Number: '))
 
+for i in range(min ,max):
+    print(i)
 
