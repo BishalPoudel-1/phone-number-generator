c3179de7d1da41d312244fabd9072de1ee328460
BishalPoudel-1 <bishalpoudel884@gmail.com>
2025-05-29 23:05:32 +0545

    come change

--- phonenumbergenerator.py
+++ phonenumbergenerator.py
@@ -10,7 +10,9 @@ firstdigit = int(input('Enter the First Digit of Phone Number: '))
 for i in range(min ,max + 1):
     start = int(str(firstdigit) + '0' * (i - 1))
     for j in range(start ,start + 1):
-        print(j)
+        j = j
+print(j)
+
 
 
 
