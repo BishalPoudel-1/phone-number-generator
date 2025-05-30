aa46e6d83ce01305444556246bae2aa3a47a3c76
BishalPoudel-1 <bishalpoudel884@gmail.com>
2025-05-30 19:26:38 +0545

    Phone generator complete

--- phonenumbergenerator.py
+++ phonenumbergenerator.py
@@ -8,30 +8,19 @@ filename = str(input("Enter the filename: "))
 
 
 
-for i in range(min ,max + 1):
+for i in range(min ,max + 1): #This loop make the first number
     start = int(str(firstdigit) + '0' * (i - len(str(firstdigit))))
     for startdigit in range(start ,start + 1):
         startdigit = startdigit
 
 
-for j in range(1, startdigit):
+for j in range(1, startdigit): #In this loop 1 is added to the first number till the last number.
     startdigit = startdigit + 1
     with open(f"{filename}phonenumber.txt", "a") as f:
         if not str(startdigit).startswith(str(firstdigit)):
             break
         f.write(str(startdigit)+"\n")
         print(startdigit)
-
-''' 
-for j in range(1, startdigit):
-    startdigit = startdigit + 1
-    with open(f"{filename}phonenumber.txt", "a") as f:
-        if len(str(startdigit)) > max:
-            break
-        f.write(str(startdigit)+"\n")
-        print(startdigit)
-
-'''
 
 print("Phone Number saved successfully")
 
