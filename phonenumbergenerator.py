d7a75df4ae1b1827dee258a266f71864f4a2c220
BishalPoudel-1 <bishalpoudel884@gmail.com>
2025-05-30 16:59:45 +0545

    done

--- phonenumbergenerator.py
+++ phonenumbergenerator.py
@@ -4,18 +4,19 @@ print("*************************************\n")
 min = int(input("Enter the minimum Digits: "))
 max = int(input("Enter the maximum Digits: "))
 firstdigit = int(input('Enter the First Digit of Phone Number: '))
+filename = str(input("Enter the filename: "))
 
 
 
 for i in range(min ,max + 1):
-    start = int(str(firstdigit) + '0' * (i - 1))
+    start = int(str(firstdigit) + '0' * (i - len(str(firstdigit))))
     for startdigit in range(start ,start + 1):
         startdigit = startdigit
 
 
 for j in range(1, startdigit):
     startdigit = startdigit + 1
-    with open("phonenumber.txt", "a") as f:
+    with open(f"{filename}phonenumber.txt", "a") as f:
         if len(str(startdigit)) > max:
             break
         f.write(str(startdigit)+"\n")
